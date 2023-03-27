7e8eb4b90518a95d5fe5f9706459479c6c6935af
Thibaut Robert <thibaut@conduktor.io>
2023-03-27 09:39:49 +0200

    retrieve user token and forwards it to rust (#13)

--- conduktor-desktop/src/App.tsx
+++ conduktor-desktop/src/App.tsx
@@ -1,52 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useEffect } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import { useAuth, withAuthenticationRequired } from "@conduktor/auth";
+import { SpinnerDots } from "@conduktor/ui-library";
 import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+const App = withAuthenticationRequired(AppMain, {
+  onRedirecting: () => <SpinnerDots />,
+})
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+function AppMain() {
+  const auth = useAuth();
+  //const orgs = useOrganizations();
+
+  useEffect(() => {
+    async function forwardToken() {
+      const token = await auth.getAccessTokenSilently();
+      await invoke("token", {token: token.access_token});
+      // const slug = orgs.currentOrganization?.slug ?? 'conduktor';
+      // console.log(`Redirecting to https://${slug}.stg.conduktor.app/`);
+      window.location.href = "https://signup.staging.conduktor.io/";
+    }
+
+    forwardToken();
+  }, [auth]);
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
-      <p>{greetMsg}</p>
-    </div>
+    <SpinnerDots />
   );
 }
 
--- conduktor-desktop/src/main.tsx
+++ conduktor-desktop/src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AuthProvider } from "@conduktor/auth";
 import App from "./App";
 import "./styles.css";
 
+const env = import.meta.env;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider mode={env.VITE_MODE}
+                  domain={env.VITE_AUTH0_DOMAIN}
+                  clientId={env.VITE_AUTH0_CLIENT_ID}
+                  redirectUri={window.location.origin}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
