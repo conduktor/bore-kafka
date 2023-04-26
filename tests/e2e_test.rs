c066daa1341418057e9c3dce08dd95cc10b19f36
Thibaut Robert <thibaut@conduktor.io>
2023-04-26 16:59:20 +0200

    Add kafka e2e test (#24)
    * Add kafka e2e test
    
    * switch to rskafka
    
    * skip integration test when cross-testing

--- tests/e2e_test.rs
+++ tests/e2e_test.rs
@@ -3,14 +3,22 @@ use std::sync::{Arc, RwLock};
 use std::time::Duration;
 
 use anyhow::{anyhow, Result};
-use conduktor_kafka_proxy::proxy_state::ProxyState;
-use conduktor_kafka_proxy::{client::Client, server::Server, shared::CONTROL_PORT};
 use lazy_static::lazy_static;
+use rskafka::client::partition::Compression;
+use rskafka::client::ClientBuilder;
+use rskafka::record;
+use rskafka::time::OffsetDateTime;
 use rstest::*;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
+use testcontainers::images::kafka;
+use testcontainers::images::kafka::Kafka;
+use testcontainers::{clients, Container};
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
 use tokio::sync::Mutex;
 use tokio::time;
+
+use conduktor_kafka_proxy::proxy_state::ProxyState;
+use conduktor_kafka_proxy::{client::Client, server::Server, shared::CONTROL_PORT};
 
 lazy_static! {
     /// Guard to make sure that tests are run serially, not concurrently.
@@ -25,50 +33,78 @@ async fn spawn_server(secret: Option<&str>) {
     time::sleep(Duration::from_millis(50)).await;
 }
 
+/// Start a Kafka container, returning the container and server port.
+fn start_kafka(docker: &clients::Cli) -> (Container<Kafka>, u16) {
+    let kafka_node = docker.run(Kafka::default());
+    let local_port = kafka_node.get_host_port_ipv4(kafka::KAFKA_PORT);
+    (kafka_node, local_port)
+}
+
 /// Spawns a client with randomly assigned ports, returning the listener and remote address.
-async fn spawn_client(secret: Option<&str>) -> Result<(TcpListener, SocketAddr)> {
-    let listener = TcpListener::bind("localhost:0").await?;
-    let local_port = listener.local_addr()?.port();
+async fn spawn_client(secret: Option<&str>, port: u16) -> Result<SocketAddr> {
     let proxy_state = Arc::new(RwLock::new(ProxyState::new(None)));
-    let client = Client::new("localhost", local_port, "localhost", 0, secret, proxy_state).await?;
+    let client = Client::new("localhost", port, "localhost", 0, secret, proxy_state).await?;
     let remote_addr = ([127, 0, 0, 1], client.remote_port()).into();
     tokio::spawn(client.listen());
-    Ok((listener, remote_addr))
+    Ok(remote_addr)
 }
 
 #[rstest]
 #[tokio::test]
+#[cfg_attr(not(feature = "integration_tests"), ignore)]
 async fn basic_proxy(#[values(None, Some(""), Some("abc"))] secret: Option<&str>) -> Result<()> {
     let _guard = SERIAL_GUARD.lock().await;
-
+    let docker = clients::Cli::default();
+    let (_kafka_node, local_port) = start_kafka(&docker);
     spawn_server(secret).await;
-    let (listener, addr) = spawn_client(secret).await?;
+    let addr = spawn_client(secret, local_port).await?;
 
-    tokio::spawn(async move {
-        let (mut stream, _) = listener.accept().await?;
-        let mut buf = [0u8; 11];
-        stream.read_exact(&mut buf).await?;
-        assert_eq!(&buf, b"hello world");
+    let bootstrap_servers = vec![addr.to_string()];
+    let topic = "test-topic";
 
-        stream.write_all(b"I can send a message too!").await?;
-        anyhow::Ok(())
-    });
+    let client = ClientBuilder::new(bootstrap_servers).build().await.unwrap();
+    let partition_client = client
+        .partition_client(
+            topic.to_owned(),
+            0, // partition
+        )
+        .unwrap();
 
-    let mut stream = TcpStream::connect(addr).await?;
-    stream.write_all(b"hello world").await?;
+    let number_of_messages_to_produce = 5;
+    let expected: Vec<String> = (0..number_of_messages_to_produce)
+        .map(|i| format!("Message {}", i))
+        .collect();
 
-    //FIXME : debug these tests
+    for (i, message) in expected.iter().enumerate() {
+        partition_client
+            .produce(
+                vec![record::Record {
+                    key: Some(format!("Key {}", i).as_bytes().into()),
+                    value: Some(message.as_bytes().into()),
+                    headers: Default::default(),
+                    timestamp: OffsetDateTime::now_utc(),
+                }],
+                Compression::NoCompression,
+            )
+            .await
+            .unwrap();
+    }
 
-    // let mut buf = [0u8; 25];
-    // stream.read_exact(&mut buf).await.expect("read exact failed");
-    // assert_eq!(&buf, b"I can send a message too!");
+    let mut offset: usize = 0;
+    while offset < number_of_messages_to_produce {
+        let consumed = partition_client
+            .fetch_records(offset as i64, 0..1_000_000, 10_000)
+            .await
+            .unwrap()
+            .0;
 
-    // Ensure that the client end of the stream is closed now.
-    // assert_eq!(stream.read(&mut buf).await?, 0);
-
-    // Also ensure that additional connections do not produce any data.
-    // let mut stream = TcpStream::connect(addr).await?;
-    // assert_eq!(stream.read(&mut buf).await?, 0);
+        let consumed: Vec<String> = consumed
+            .into_iter()
+            .map(|r| String::from_utf8(r.record.value.unwrap()).unwrap())
+            .collect();
+        assert_eq!(consumed, expected[offset..offset + consumed.len()]);
+        offset += consumed.len();
+    }
 
     Ok(())
 }
@@ -84,7 +120,7 @@ async fn mismatched_secret(
     let _guard = SERIAL_GUARD.lock().await;
 
     spawn_server(server_secret).await;
-    assert!(spawn_client(client_secret).await.is_err());
+    assert!(spawn_client(client_secret, 0).await.is_err());
 }
 
 #[tokio::test]
